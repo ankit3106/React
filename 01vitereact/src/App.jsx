58638b56de6eff23646768922ba259308bae268f
Ankit <luffytarow13@gmail.com>
2024-07-05 14:26:54 +0530

    hooks

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Script from "./Script";
+
+function App() {
+  const username = "chai aur code";
+  return (
+    <>
+      <Script />
+      <h1>Chai aur react {username}</h1>
+      <p>test para</p>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/Script.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Script = () => {
+  return (
+    <div>
+      <h1>script</h1>
+    </div>
+  )
+}
+
+export default Script
