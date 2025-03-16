28163a40ca749cfd0ecbaf9618782fdc8aecc1c5
Ankit <luffytarow13@gmail.com>
2025-03-16 19:58:54 +0530

    components done and auth and conf done

--- BlogApp/src/appwrite/auth.js
+++ BlogApp/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from "../conf/conf";
+import conf from "../conf/conf.js";
 
 import { Client, Account, ID } from "appwrite";
 
@@ -37,12 +37,11 @@ export class AuthService {
         }
     }
 
-    async getCurrentUser(){
+    async getCurrentUser() {
         try {
             return await this.account.get();
-            
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
 
         return null;
